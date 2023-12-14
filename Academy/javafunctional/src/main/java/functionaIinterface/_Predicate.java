71b00af883373681947576ea93ffc314b8205a89
Ventsislav Peychev <del.busque@gmail.com>
2023-12-14 11:51:08 +0200

    Add BiPredicate

--- Academy/javafunctional/src/main/java/functionaIinterface/_Predicate.java
+++ Academy/javafunctional/src/main/java/functionaIinterface/_Predicate.java
@@ -1,5 +1,6 @@
 package functionaIinterface;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class _Predicate {
@@ -8,8 +9,14 @@ public class _Predicate {
         System.out.println(isPhoneNumValid.test("+341234567"));
         System.out.println(isPhoneNumValid.and(contains3).test("+441234567"));
         System.out.println(isPhoneNumValid.or(contains3).test("+431234567"));
+
+        Boolean result = isIt.test(true, 190);
+        System.out.println("It is: " + result);
     }
 
     static Predicate<String> isPhoneNumValid = num -> num.length() == 10 && num.startsWith("+44");
     static Predicate<String> contains3 = num -> num.contains("3");
+
+    static BiPredicate<Boolean, Integer> isIt = (flag, num) -> flag && num * num == 100;
+
 }
