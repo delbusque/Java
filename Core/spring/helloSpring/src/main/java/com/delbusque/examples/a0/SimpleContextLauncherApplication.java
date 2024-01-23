9938da3a241462d1ebb5979c6ee1e5ba00280ed3
delbusque <del.busque@gmail.com>
2024-01-23 16:22:50 +0200

    Add Field dependency injection

---
+++ Core/spring/helloSpring/src/main/java/com/delbusque/examples/a0/SimpleContextLauncherApplication.java
@@ -0,0 +1,19 @@
+package com.delbusque.examples.a0;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class SimpleContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext(SimpleContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
